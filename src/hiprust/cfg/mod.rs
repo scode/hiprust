f1afe71d183fce37847671436330cd7fe0873804
Peter Schuller <peter.schuller@infidyne.com>
2014-09-21 03:11:12 +0000

    Test reading config from file.

--- src/hiprust/cfg/mod.rs
+++ src/hiprust/cfg/mod.rs
@@ -1,4 +1,5 @@
 use serialize::json;
+use std::io;
 
 #[deriving(Decodable, Encodable)]
 pub struct Config {
@@ -11,15 +12,38 @@ fn from_string(s: &str) -> Config {
 
 }
 
+fn from_file(p: &Path) -> Config {
+    from_string(io::BufferedReader::new(io::File::open(p)).read_to_string().unwrap().as_slice())
+}
+
 #[cfg(test)]
 mod test {
-    use super::from_string;
+    use std::io;
+
+    fn valid_cfg_json() -> &'static str {
+        "{\
+            \"auth_token\": \"test\",\
+            \"api_host\": \"api.hipchat.com\"\
+         }"
+    }
 
     #[test]
     fn test_from_string() {
-        from_string("{\
-                     \"auth_token\": \"test\",\
-                     \"api_host\": \"api.hipchat.com\"\
-                    }");
+        super::from_string(valid_cfg_json());
+    }
+
+    #[test]
+    fn test_from_file() {
+        // XXX(scode): The hoop jumping to create tmp_path seems wrong and ugly.
+        let tmp_dir = io::TempDir::new("jsontest").unwrap();
+        let mut tmp_path = Path::new(tmp_dir.path());
+        tmp_path.push("tmpcfg");
+
+        let mut tmp_out = io::BufferedWriter::new(io::File::open_mode(&tmp_path, io::Open, io::Write)).unwrap();
+
+        tmp_out.write_str(valid_cfg_json()).unwrap();
+        tmp_out.flush().unwrap();
+
+        super::from_file(&tmp_path);
     }
 }
